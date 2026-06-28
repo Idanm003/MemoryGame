bf76ea04870a503846ccd0062062524714f4a9fc
IdanMagen <idanmagen234@gmail.com>
2026-06-28 23:23:12 +0300

    Readded the config file

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    MAX_PAIRS: 8,
+    GRID_COLUMNS: 4,
+    UNFLIP_DELAY_MS: 1500,
+    COUNTRIES_LIMIT: 100,
+
+    API_URLS: {
+        HARRY_POTTER: "https://hp-api.onrender.com/api",
+        DOGS: "https://dog.ceo/api",
+        COUNTRIES: "https://api.restcountries.com/countries/v5",
+    },
+
+    THEMES: ["Harry Potter", "Dogs", "Countries", "Random"],
+
+    COUNTRIES_API_KEY: "rc_live_2c3d4cb8ba0841888a8936d14d07fccb",
+};
